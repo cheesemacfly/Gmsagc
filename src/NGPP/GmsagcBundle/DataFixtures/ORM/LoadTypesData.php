3a4491ada13a871fa1bf53e700968ff4920c54af
nicolas <nicolas@nbox>
2013-04-11 15:15:58 -0400

    Move Types values to config file

--- src/NGPP/GmsagcBundle/DataFixtures/ORM/LoadTypesData.php
+++ src/NGPP/GmsagcBundle/DataFixtures/ORM/LoadTypesData.php
@@ -5,17 +5,33 @@ namespace NGPP\GmsagcBundle\DataFixtures\ORM;
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use NGPP\GmsagcBundle\Entity\Types;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
-class LoadTypesData implements FixtureInterface
+class LoadTypesData implements FixtureInterface, ContainerAwareInterface
 {
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
     /**
      * {@inheritDoc}
      */
     public function load(ObjectManager $manager)
     {
-        $manager->persist(new Types(1, 'OWNER'));
-        $manager->persist(new Types(2, 'CUSTOMER'));
-        $manager->persist(new Types(3, 'PROVIDER'));
+        foreach($this->container->getParameter('ngpp_gmsagc_types') as $key => $value)
+        {            
+            $manager->persist(new Types($value, $key));
+        }
         $manager->flush();
     }
 }
