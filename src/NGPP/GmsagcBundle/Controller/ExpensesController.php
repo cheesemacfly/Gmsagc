f94f0f7bf1ae8a2d621c71fe3f9329ab60113825
Nicolas Guelpa <nicolas@guelpa.me>
2013-10-17 15:47:01 -0400

    Fix critical/major issues from https://insight.sensiolabs.com/projects/0ed9f64c-0d74-44ea-9e32-2ff4b8c055b4

--- src/NGPP/GmsagcBundle/Controller/ExpensesController.php
+++ src/NGPP/GmsagcBundle/Controller/ExpensesController.php
@@ -22,18 +22,18 @@ class ExpensesController extends Controller
     public function indexAction($order_id)
     {
         $em = $this->getDoctrine()->getManager();
-        
+
         if(is_null($order = $em->getRepository('NGPPGmsagcBundle:Orders')->find($order_id)))
         {
             $this->get('session')->getFlashBag()->add('error',
                     $this->get('translator')->trans('expenses.noorder'));
             return $this->redirect($this->generateUrl('ngpp_gmsagc_home'));
         }
-        
+
         return array('expenses' => $this->getDoctrine()->getRepository('NGPPGmsagcBundle:Expenses')->findByOrder($order->getId()),
                       'order' => $order);
     }
-    
+
     /**
      * @Route("/create/{order_id}", name="ngpp_gmsagc_expenses_create", requirements={"order_id" = "\d+"})
      * @ParamConverter("order", class="NGPPGmsagcBundle:Orders", options={"id" = "order_id"})
@@ -42,11 +42,11 @@ class ExpensesController extends Controller
     public function createAction(Orders $order)
     {
         $em = $this->getDoctrine()->getManager();
-        
+
         $expense = new Expenses($order);
         $form = $this->createForm(new ExpensesType(), $expense);
         $form->handleRequest($this->getRequest());
-        
+
         if ($form->isValid()) {
 
             $em->persist($expense);
@@ -57,21 +57,21 @@ class ExpensesController extends Controller
 
             return $this->redirect($this->generateUrl('ngpp_gmsagc_expenses', ['order_id' => $order->getId()]));
         }
-        
+
         return array('form' => $form->createView(), 'order_id' => $order->getId());
     }
-    
+
     /**
      * @Route("/edit/{id}", name="ngpp_gmsagc_expenses_edit", requirements={"id" = "\d+"})
      * @Template("NGPPGmsagcBundle:Expenses:save.html.twig")
      */
     public function editAction(Expenses $expense)
-    {        
+    {
         $em = $this->getDoctrine()->getManager();
-        
+
         $form = $this->createForm(new ExpensesType(), $expense);
         $form->handleRequest($this->getRequest());
-            
+
         if ($form->isValid()) {
 
             $em->persist($expense);
@@ -82,10 +82,10 @@ class ExpensesController extends Controller
 
             return $this->redirect($this->generateUrl('ngpp_gmsagc_expenses'));
         }
-        
+
         return array('form' => $form->createView(), 'order_id' => $expense->getOrderId());
     }
-    
+
     /**
      * @Route("/delete/{id}", name="ngpp_gmsagc_expenses_delete", requirements={"id" = "\d+"})
      */
@@ -93,23 +93,20 @@ class ExpensesController extends Controller
     {
         $em = $this->getDoctrine()->getManager();
         $expense = $em->getRepository('NGPPGmsagcBundle:Expenses')->find($id);
-        
-        $redirect_url = $this->generateUrl('ngpp_gmsagc_home');
 
+        $redirect_url = $this->generateUrl('ngpp_gmsagc_home');
         if(!is_null($expense))
         {
             $redirect_url = $this->generateUrl('ngpp_gmsagc_expenses', ['order_id' => $expense->getOrderId()]);
-            
+
             $em->remove($expense);
             $em->flush();
-            
-            $this->get('session')->getFlashBag()->add('success',
-                    $this->get('translator')->trans('expenses.deleted'));
+
+            $this->get('session')->getFlashBag()->add('success', $this->get('translator')->trans('expenses.deleted'));
         }
         else
-            $this->get('session')->getFlashBag()->add('error',
-                    $this->get('translator')->trans('expenses.nodeleted'));
-        
+            $this->get('session')->getFlashBag()->add('error', $this->get('translator')->trans('expenses.nodeleted'));
+
         return $this->redirect($redirect_url);
     }
 }
